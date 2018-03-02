42d1cfaa679d4cf40d6857e74d42fc5796524150
Shady Khalifa <shekohex@gmail.com>
2018-03-02 23:21:52 +0200

    update routes

--- routes/index.go
+++ routes/index.go
@@ -7,15 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var routesMap map[string]string
+var routesMap map[string]interface{}
 
 // Index for the route '/'
 func Index(c *gin.Context) {
-	routesMap = make(map[string]string)
-	routesMap["GET"] = "/"
-	routesMap["POST"] = "/feedbacks/create"
-	routesMap["GET"] = "/feedbacks"
-	routesMap["POST"] = "/feedbacks/reply/:feedbackId"
+	routesMap = make(map[string]interface{})
+	routesMap["/"] = "[GET] The Root Route"
+	routesMap["/feedbacks/create"] = "[POST] Create New Feedback"
+	routesMap["/feedbacks"] = "[GET] all feedbacks"
+	routesMap["/feedbacks/reply/:feedbackId"] = "[POST] Create New Reply on Feedback with id=feedbackId"
 
 	c.JSON(http.StatusOK,
 		gin.H{"status": "OK",
