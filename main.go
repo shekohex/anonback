61372c03958bfc97fcec1bfbf34fdd3016a74cde
Shady Khalifa <shekohex@gmail.com>
2018-03-02 13:39:21 +0200

    hello world

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func helloWorld(c *gin.Context) {
+	c.String(200, "Hello World")
+}
+func setupRouter() (app *gin.Engine) {
+	// gin.DisableConsoleColor()
+	app = gin.Default()
+	// Ping test
+	app.GET("/", helloWorld)
+	return
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	app := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	app.Run(":8080")
+	fmt.Println("Listening on :8080")
+}
